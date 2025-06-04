4e6c9a906168b0388265e8bb07e94dc7c6d4a288
Muhammed ALi <155841427+adilali097@users.noreply.github.com>
2025-06-04 11:18:30 +0530

    Create calculator_cli.py

---
+++ Myprojectsfornothing/calculator_cli.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("Simple Calculator")
+    print("Operations: +, -, *, /")
+    a = float(input("Enter first number: "))
+    op = input("Enter operator: ")
+    b = float(input("Enter second number: "))
+
+    if op == "+":
+        print("Result:", a + b)
+    elif op == "-":
+        print("Result:", a - b)
+    elif op == "*":
+        print("Result:", a * b)
+    elif op == "/":
+        print("Result:", a / b)
+    else:
+        print("Invalid operator")
+
+if __name__ == '__main__':
+    calculator()
